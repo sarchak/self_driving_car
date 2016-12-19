6a93b30c03d6db7a1525ebe94ed32d7b4422c694
Shrikar <shrikar84@gmail.com>
2016-12-18 17:57:38 -0800

    Try smaller network

--- CarND-Traffic-Signs/cnn_gpu.py
+++ CarND-Traffic-Signs/cnn_gpu.py
@@ -33,20 +33,20 @@ def max_pool_2x2(x):
                         strides=[1, 2, 2, 1], padding='SAME')
 
 def conv_net(x, weights, biases, keep_prob):
-    W_conv1 = weight_variable([5, 5, 1, 64])
-    b_conv1 = bias_variable([64])
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
 
     h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
-    W_conv2 = weight_variable([5, 5, 64, 128])
-    b_conv2 = bias_variable([128])
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
-    W_conv3 = weight_variable([5, 5, 128, 256])
-    b_conv3 = bias_variable([256])
+    W_conv3 = weight_variable([5, 5, 64, 128])
+    b_conv3 = bias_variable([128])
 
     h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
     h_pool3 = max_pool_2x2(h_conv3)
@@ -54,10 +54,10 @@ def conv_net(x, weights, biases, keep_prob):
     #W_fc1 = weight_variable([8 * 8 * 128, 1024])
     #b_fc1 = bias_variable([1024])
 
-    W_fc1 = weight_variable([4 * 4 * 256, 1024])
+    W_fc1 = weight_variable([4 * 4 * 128, 1024])
     b_fc1 = bias_variable([1024])
 
-    h_pool3_flat = tf.reshape(h_pool3, [-1, 4 * 4 * 256])
+    h_pool3_flat = tf.reshape(h_pool3, [-1, 4 * 4 * 128])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
 
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
@@ -107,7 +107,7 @@ image_shape = X_train[0].shape
 # TODO: How many unique classes/labels there are in the dataset.
 n_classes = y_train.shape[1]
 
-batch_size =  64 
+batch_size =  64
 
 x = tf.placeholder("float", [None, 32, 32, 1])
 y = tf.placeholder("float", [None, n_classes])
